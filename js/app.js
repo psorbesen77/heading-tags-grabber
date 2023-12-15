17f8f6c2d5c53ab8c68a9b489a691ac64ea0c554
Patiphan Orbesen <porbesen09@gmail.com>
2023-12-14 23:46:15 -0700

    Finish with app.js

--- js/app.js
+++ js/app.js
@@ -26,9 +26,32 @@ function grabHeader () {
             const parser = new DOMParser();
             // Parse the HTML content into a DOM document 
             const doc = parser.parseFromString(html, "text/html");
-            
-            
 
+            // Select all heading tags from the parsed document
+            const headings = doc.querySelectorAll("h1, h2, h3, h4, h5, h6");
+
+            const resultDiv = document.getElementById("result");
+
+            // Check if not heading are found
+            if (headings.length === 0) {
+                resultDiv.innerHTML = "<p>No heading found on the page. </p>";
+            } else {
+
+                // If heading are found, create a list and display them in the result div
+                let resultHTML = "<p>Headers Found: </p><ul>";
+                headings.forEach(heading => {
+                    resultHTML += `<li>${heading.innerHTML}</li>`;
+                });
+
+                resultHTML += "</ul>";
+                resultDiv.innerHTML = resultHTML;
+
+            }     
+            
         })
-
+        // Handle errors during the fetch operation
+        .catch(error => {
+            console.error("Error fetching the URL:", error);
+            alert("An error occurred while fetching the URL. Please try again.");
+        });
 }
